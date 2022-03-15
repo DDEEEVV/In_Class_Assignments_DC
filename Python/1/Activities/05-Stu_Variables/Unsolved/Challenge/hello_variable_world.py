724d9eb7daf7d5223d7f8caf984f84f1a1dbb540
DDEEEVV <davit.cuedari@gmail.com>
2022-03-15 15:12:53 -0400

    more pythoning around

--- Python/1/Activities/05-Stu_Variables/Unsolved/Challenge/hello_variable_world.py
+++ Python/1/Activities/05-Stu_Variables/Unsolved/Challenge/hello_variable_world.py
@@ -6,15 +6,19 @@
 
 # Create float variable for original_price
 
+original_price = 198.87
 
 # Create float variable for current_price
 
+current_price = 254.32
 
 # Calculate difference between current_price and original_price
 
+increase = current_price - original_price
 
 # Calculate percent_increase
 
+percent_increase = increase / current_price * 100
 
 # Print original_price
 print(f"Apple's original stock price was ${original_price}")
@@ -23,3 +27,4 @@ print(f"Apple's original stock price was ${original_price}")
 print(f"Apples current stock price is ${current_price}")
 
 # Print percent_increase to 2 decimal places using string formatting
+print(f"Apples percentage increase price is % {round(percent_increase,2)}")
