600b6c69e9a894b7485260b1db204a0ce28afe79
DDEEEVV <davit.cuedari@gmail.com>
2022-03-15 14:56:34 -0400

    some python edittings

--- Python/1/Activities/02-Ins_Jupyter_Project/hello_world.py
+++ Python/1/Activities/02-Ins_Jupyter_Project/hello_world.py
@@ -2,3 +2,7 @@
 print('Hello World!')
 # print a statement: 'Hello World from a new cell'
 print('Hello World from a new cell')
+
+print('What a beautiful day! March 15, 2022')
+print('My first hands on Python and it feels great!')
+print('I will fucking ace this!')
