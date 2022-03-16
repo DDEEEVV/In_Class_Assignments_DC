99649c6286d75023caa7ce6486b6c7ddcd2aee6d
DDEEEVV <davit.cuedari@gmail.com>
2022-03-15 23:45:24 -0400

    Cheer exercise

--- Python/1/Activities/09-Stu_Loops/Unsolved/cheer.py
+++ Python/1/Activities/09-Stu_Loops/Unsolved/cheer.py
@@ -1,12 +1,23 @@
 # Create a variable named cheer and give it a word to cheer (i.e. Python or FinTech)
-
+cheer = "Python" 
 
 # Below strings can be used to add fun
 cheer_symbol = "*\O/*"
 cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
 
 # Loop through string and print each letter with a cheer
-
+print("Give me a P!")
+print("P")
+print("Give me a y!")
+print("y")
+print("Give me a t!")
+print("t")
+print("Give me a h!")
+print("h")
+print("Give me a o!")
+print("o")
+print("Give me a n!")
+print("n")
 
 # Print excitement to screen
 print("\nWhat does that spell?!")
